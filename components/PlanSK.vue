c25e2e15336610a40a845d8a1a791dc911f4d741
Nikola Irakoski <67819241+irak99@users.noreply.github.com>
2021-05-19 10:09:39 +0200

    odvoeni podatoci i karticki

---
+++ components/PlanSK.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <p>{{ test }}</p>
+  </div>
+</template>
+
+<script>
+import data from 'static/GodisenPlanSK2021'
+export default {
+  data() {
+    return {
+      test: [],
+    }
+  },
+}
+</script>
+
+<style></style>
